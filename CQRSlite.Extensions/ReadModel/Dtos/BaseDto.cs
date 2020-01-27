bee7c5ad3749f5e1e5147c18c77251a6e9631028
barimale <mateusz.wolnica@gmail.com>
2020-01-27 14:44:35 +0100

    Guid added to baseDto.

--- CQRSlite.Extensions/ReadModel/Dtos/BaseDto.cs
+++ CQRSlite.Extensions/ReadModel/Dtos/BaseDto.cs
@@ -1,9 +1,17 @@
 ﻿using CQRSlite.Extensions.Common;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CQRSlite.Extensions.ReadModel.Dtos
 {
     public abstract class BaseDto : ISoftDelete, IPersonified
     {
+        [Key, Column(Order = 0)]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public Guid Guid { get; set; }
+        [Key, Column(Order = 1)]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int Version { get; set; }
         public string ExecutorId { get; set; }
         public bool IsDeleted { get; set; }
